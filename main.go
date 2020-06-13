1b1deb1cfc8979f4719dd1e2c400692e1414b770
JiayouZ116 <zzzjy1995@gmail.com>
2020-06-13 16:23:42 -0400

    URL update

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ const (
 	//PROJECT_ID = "around-xxx"
 	//BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://34.71.224.246:9200"
+	ES_URL = "http://35.223.9.166:9200"
 )
 
 func main() {
